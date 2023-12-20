56658aedb14d70e571cecbb1c66e35f4a013ea91
PrasunSh <prasundummy@gmail.com>
2023-12-20 19:29:29 +0530

    some CSS

--- dojo-blogs/src/NotFound.js
+++ dojo-blogs/src/NotFound.js
@@ -5,7 +5,9 @@ const NotFound = () => {
     <div className="not-found">
       <h2>Sorry</h2>
       <p>That page cannot be found</p>
-      <Link to="/">Back to the homepage...</Link>
+      
+      <div className="not-found"><Link to="/">Back to the homepage...</Link>
+      </div>
     </div>
   );
 }
